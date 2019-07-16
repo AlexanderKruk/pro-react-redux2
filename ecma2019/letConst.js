fe65a1023682e22cd28b0812061ce685758f86cf
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-07-16 17:27:29 +0300

    new app

---
+++ ecma2019/letConst.js
@@ -0,0 +1,18 @@
+let name = 'Joe';
+name = 'Bob';
+console.log(name);
+
+const pi = 3.14;
+
+const names = ['Peter', 'Bob'];
+
+names.push('Jenny');
+console.log(names);
+
+const person = {
+  name: 'Joe',
+  age: 25
+}
+
+person.name = 'Bob';
+person = "Alice";
