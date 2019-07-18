221ae2da2ce147d737517443a7e39c8569ccf4b4
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-07-18 12:59:27 +0300

    25 React Components

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// JSX -> Babel -> Clean JS
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input placeholder='search'></input>
-    <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
-    </ul>
+const TodoList = () => {
+  return (    
+  <ul>
+    <li>Learn React</li>
+    <li>Build Awesome App</li>
+  </ul>);
+};
+
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+  return <input placeholder='search'></input>;
+}
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
   </div>
-);
+  );
+}
+
+// JSX -> Babel -> Clean JS
+// const el = <App/>;
 // Clean JS, light object
 // const el = React.createElement('h1', null, 'Hello World!');
 
-ReactDOM.render(el, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
