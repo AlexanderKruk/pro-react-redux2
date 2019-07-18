8c8fd67f1f4ddf7810f45c9819c8896807e0c0a0
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-07-18 12:39:55 +0300

    24. React elements

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// JSX -> Babel -> Clean JS
+const el = (
+  <div>
+    <h1>My Todo List</h1>
+    <input placeholder='search'></input>
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>
+);
+// Clean JS, light object
+// const el = React.createElement('h1', null, 'Hello World!');
+
+ReactDOM.render(el, document.getElementById('root'));
