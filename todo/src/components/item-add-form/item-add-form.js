aedc88065b1be157d6aa3ff38dec5955f7eeec27
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-07-20 17:57:53 +0300

    45. Controlled component

--- todo/src/components/item-add-form/item-add-form.js
+++ todo/src/components/item-add-form/item-add-form.js
@@ -11,6 +11,9 @@ export default class ItemAddForm extends Component {
   onSubmit = (e) => {
     e.preventDefault();
     this.props.onAddItem(this.state.label);
+    this.setState({
+      label: ''
+    })
   }
 
   onLabelChange = (e) => {
@@ -20,16 +23,15 @@ export default class ItemAddForm extends Component {
   }
 
   render() {
-
-    const { onAddItem } = this.props;
-
+    
     return(
       <form className="item-add-form d-flex"
             onSubmit={ this.onSubmit }>
         <input type="text"
                className="form-control"
                onChange={ this.onLabelChange }
-               placeholder="New task"/>
+               placeholder="New task"
+               value={ this.state.label }/>
         <button className="btn btn-outline-secondary">
           Add
         </button>
