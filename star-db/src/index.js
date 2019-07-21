e2d7410c7c3385e4f5d1fc07312128fbc29af99c
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-07-21 18:42:53 +0300

    54. Create client for API

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,19 +1,50 @@
-const getResource = async (url) => {
-  const res = await fetch(url);
+class SwapiService {
 
-  if(!res.ok) {
-    throw new Error(`Server not have this source in ${url}` +
-                    ` status ${res.status}`);
+  _urlBase = 'https://swapi.co/api';
+
+  async getResource(url) {
+    const res = await fetch(url);
+
+    if(!res.ok) {
+      throw new Error(`Server not have this source in ${url}` +
+                      ` status ${res.status}`);
+    }
+    
+    const body = await res.json();
+    return body;  
+  }
+  
+  async getAllPeople() {
+    const res = await this.getResource(`${this._urlBase}/people`);
+    return res.results;
   }
 
-  const body = await res.json();
-  return body;
+  getPerson(id) {
+    return this.getResource(`${this._urlBase}/people/${id}`);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource(`${this._urlBase}/starships`);
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResource(`${this._urlBase}/starships/${id}`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource(`${this._urlBase}/planets`);
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`${this._urlBase}/planets/${id}`);
+  }
+
 }
 
-getResource('https://swapi.co/api/people/123123/')
-  .then((body) => {
-    console.log(body);
-  })
-  .catch((err) => {
-    console.log('Network not work', err);
-  })
+const swapi = new SwapiService();
+
+swapi.getPlanet(3).then((p) => {
+  console.log(p.name);
+});
