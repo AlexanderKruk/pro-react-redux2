8bd8c68b0bfcc0843a8e314ddb91b6e81481eb87
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-07-21 17:23:14 +0300

    53. Errors in Fetch API

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,10 +1,19 @@
 const getResource = async (url) => {
   const res = await fetch(url);
+
+  if(!res.ok) {
+    throw new Error(`Server not have this source in ${url}` +
+                    ` status ${res.status}`);
+  }
+
   const body = await res.json();
   return body;
 }
 
-getResource('https://swapi.co/api/people/1/')
+getResource('https://swapi.co/api/people/123123/')
   .then((body) => {
     console.log(body);
+  })
+  .catch((err) => {
+    console.log('Network not work', err);
   })
