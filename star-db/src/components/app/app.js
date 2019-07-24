9164b1eb71acd9c880de12d16e2846e261063887
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-07-24 10:27:43 +0300

    69. componentDidCatch()

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -4,24 +4,41 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
+import ErrorButton from '../error-button';
+import ErrorIndicator from '../error-indicator';
 
 import './app.css';
 
 export default class App extends Component {
 
   state = {
-    selectedId: 5
+    selectedId: 5,
+    showRandomPlanet: true,
+    hasError: false
   }
 
   onChangeItem = (selectedId) => {
     this.setState({ selectedId });
   }
 
+  componentDidCatch() {
+    console.log('componentDidCatch()');
+    this.setState({ hasError: true });
+  }
+
   render() {
+
+    if (this.state.hasError) {
+      return  <ErrorIndicator />
+    }
+
     return(
       <div>
         <Header />
         <RandomPlanet />
+        <div className="row mb2 button-row">
+          <ErrorButton />
+        </div>
         <div className="row mb2">
           <div className="col-md-6">
             <ItemList onChangeItem = { this.onChangeItem } />
