827972ffadbb87af17f6976c947ae3314d1560fa
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-08-01 07:25:03 +0300

    88. Refactroring HOC

--- star-db/src/components/sw-components/item-list.js
+++ star-db/src/components/sw-components/item-list.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ItemList from '../item-list';
 import { withData, withSwapiService } from '../hoc-helpers';
 
-const withChildFunction = (Wrapped, fn) => {
+const withChildFunction = (fn) => (Wrapped) => {
   return (props) => {
     return(
     <Wrapped {...props}>
@@ -33,14 +33,20 @@ const mapPlanetMethodsToProps = (swapiService) => {
   };
 };
 
-const PersonList = withSwapiService(withData(
-                   withChildFunction(ItemList, renderName)), mapPersonMethodsToProps);
+const PersonList = withSwapiService(mapPersonMethodsToProps)(
+                    withData(
+                      withChildFunction(renderName)(
+                        ItemList)));
 
-const StarshipList = withSwapiService(withData(
-                     withChildFunction(ItemList, renderNameAndLenght)), mapStarshipMethodsToProps);
+const StarshipList = withSwapiService(mapStarshipMethodsToProps)(
+                      withData(
+                        withChildFunction(renderNameAndLenght)( 
+                          ItemList)));
 
-const PlanetList = withSwapiService(withData(
-                   withChildFunction(ItemList, renderName)), mapPlanetMethodsToProps);
+const PlanetList = withSwapiService(mapPlanetMethodsToProps)(
+                    withData(
+                      withChildFunction(renderName)(
+                        ItemList)));
 
 export {
   PersonList,
