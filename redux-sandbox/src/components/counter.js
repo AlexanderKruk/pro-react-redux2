19a103a279204c67833b2877e7a5684bfbc88a09
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-08-07 19:01:05 +0300

    116. Function mapDispatchToProps()

--- redux-sandbox/src/components/counter.js
+++ redux-sandbox/src/components/counter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../actions';
+import { bindActionCreators } from 'redux';
 
 const Counter = ({ counter, inc, dec , rnd}) => {
   return (
@@ -21,4 +23,18 @@ const mapStateToProps = (state) => {
   }
 };
 
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = (dispatch) => {
+
+  const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+  return {
+    inc,
+    dec,
+    rnd: () => {
+      const payload = Math.floor(Math.random() * 10);
+      rnd(payload);
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
