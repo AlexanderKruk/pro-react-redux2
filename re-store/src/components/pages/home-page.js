df09c79448ab6361c7b260ed4a605d7fa0e3be16
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-08-10 10:33:53 +0300

    125. Read data from Redux Store

--- re-store/src/components/pages/home-page.js
+++ re-store/src/components/pages/home-page.js
@@ -3,21 +3,7 @@ import BookList from '../book-list';
 
 export const HomePage = () => {
 
-  const books = [
-    {
-      id: 1,
-      title: 'Production-Ready Microservices',
-      author: 'Susan J. Fowler' 
-    },
-    {
-      id: 2,
-      title: 'Release It!',
-      author: 'Michael T. Nygard'
-    }
-  ];
-
-
   return (
-    <BookList books={books}/>
+    <BookList />
   );
 }
