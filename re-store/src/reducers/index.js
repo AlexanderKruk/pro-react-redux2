dc5d76e3bbe408b618355583b8129d87dcc8abe6
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-08-14 11:00:03 +0300

    134. Connect new component to Redux

--- re-store/src/reducers/index.js
+++ re-store/src/reducers/index.js
@@ -1,24 +1,42 @@
 const initialStore = {
   books: [],
   loading: true,
-  error: null
+  error: null,
+  cartItems: [
+    {
+      id: 1,
+      name: "Book 1",
+      count: 2,
+      total: 40
+    },
+    {
+      id: 2,
+      name: "Book 2",
+      count: 4,
+      total: 120
+    }
+  ],
+  orderTotal: 220
 }
 
 const reducer = (store = initialStore, action) => {
   switch(action.type) {
     case 'FETCH_BOOKS_REQUEST':
       return {
+        ...store,
         books: store.books,
         loading: true,
         error: null
       };
     case 'FETCH_BOOKS_SUCCESS':
       return { 
+        ...store,
         books: action.payload,
         loading: false,
         error: null };
     case 'FETCH_BOOKS_FAILURE':
       return {
+        ...store,
         books: [],
         loading: false,
         error: action.payload
